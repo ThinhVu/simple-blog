3bb13e1f9b59848dfdcad7a3977ba248db2699f0
ThinhVu <vutrongthinhk7@gmail.com>
2023-11-17 00:47:58 +0700

    fix express

--- be/express/src/index.ts
+++ be/express/src/index.ts
@@ -1,9 +1,15 @@
-import app from './app'
+import createApp from './app'
 import {createServer} from 'http';
 
 process.on('uncaughtException', function (err) {
    console.error((err && err.stack) ? err.stack : err)
 })
-const PORT = process.env.PORT || process.env.API_PORT
-const httpServer = createServer(app)
-httpServer.listen({port: PORT}, () => console.log(`httpServer ready at http://localhost:${PORT}`))
+
+async function main() {
+   const app = await createApp()
+   const PORT = process.env.PORT || process.env.API_PORT
+   const httpServer = createServer(app)
+   httpServer.listen({port: PORT}, () => console.log(`httpServer ready at http://localhost:${PORT}`))
+}
+
+main()
