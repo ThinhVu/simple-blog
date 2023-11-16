8be6039c7a6bb4b40dd11f234c0514324c5b0781
ThinhVu <vutrongthinhk7@gmail.com>
2023-11-17 00:45:26 +0700

    update express

--- be/express/src/app.ts
+++ be/express/src/app.ts
@@ -3,11 +3,11 @@ import express from 'express';
 import cors from 'cors';
 import compression from 'compression';
 import cookieParser from 'cookie-parser';
-import router from './routes';
+import routers from './routes';
 import initDb from './utils/connection';
 
 export default async function createApp() {
-  dotenv.config();
+  dotenv.config()
   await initDb()
   const app = express()
   app.use(compression())
@@ -15,6 +15,6 @@ export default async function createApp() {
   app.use(cors())
   app.use(express.json({limit: '50mb'}))
   app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}))
-  app.use('/', router)
+  app.use('/', routers)
   return app;
 }
