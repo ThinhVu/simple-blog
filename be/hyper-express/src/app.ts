9bce49f758b353576e3678b2c7bbcbd691053589
ThinhVu <thinhvux@protonmail.com>
2023-12-22 12:48:53 +0700

    hyper-express with socket.io

--- be/hyper-express/src/app.ts
+++ be/hyper-express/src/app.ts
@@ -1,10 +1,20 @@
 import cors from 'cors';
 import router from './routes';
 import {Server} from 'hyper-express';
+import {Server as SocketIoServer} from 'socket.io';
 
 export default async function () {
   const app = new Server()
   app.use(cors())
   app.use('/', router)
+  const io = new SocketIoServer({
+    cors: {origin: "http://localhost:8080"}
+  })
+  io.attachApp(app.uws_instance)
+  // @ts-ignore
+  io.on("connection", (socket) => {
+    console.log('new socket received', socket)
+    socket.emit('hi-mom', 'hyper-express')
+  });
   return app
 }
